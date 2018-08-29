da5cd39fa52689edf368b10a550c140f99846489
kamblack <me@kam.black>
2018-08-29 11:51:15 -0600

    Initial commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+            <div class="clear"></div>
+        </div>
+        <footer id="footer" role="contentinfo">
+            <div id="copyright">
+                <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'grimmoire' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'grimmoire' ), '<a href="https://kam.black/">Kam Black</a>' ); ?>
+            </div>
+        </footer>
+    </div>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
